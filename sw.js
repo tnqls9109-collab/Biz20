6d9fa43ca68923fb1e08b45ad7e1a7df4e4bb954
tnqls9109-collab <tnqls9109@nate.com>
2026-06-26 11:20:58 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = 'biz-english-v2';
+const CACHE_NAME = 'biz-english-v3-routine';
 const ASSETS_TO_CACHE = [
-  './TalkFile_business_english_fillblank_v2.html.html',
+  './index.html',
   './manifest.json',
   './icon.png'
 ];
@@ -10,21 +10,21 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('앱 리소스를 캐싱합니다.');
+        console.log('새로운 루틴 앱 리소스를 캐싱합니다.');
         return cache.addAll(ASSETS_TO_CACHE);
       })
       .then(() => self.skipWaiting())
   );
 });
 
-// 활성화 단계: 오래된 캐시 삭제
+// 활성화 단계: 오래된 구버전 캐시 삭제
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cache => {
           if (cache !== CACHE_NAME) {
-            console.log('이전 캐시를 삭제합니다:', cache);
+            console.log('이전 구버전 캐시를 삭제합니다:', cache);
             return caches.delete(cache);
           }
         })
@@ -33,7 +33,7 @@ self.addEventListener('activate', event => {
   );
 });
 
-// 네트워크 요청 가로채기: 네트워크가 끊겨도 오프라인 캐시본 반환
+// 네트워크 요청 가로채기: 오프라인 대응
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
